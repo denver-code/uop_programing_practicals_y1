0e553ffe197121ac91417490c51b0239a5664607
Ihor Savenko <csigorek@gmail.com>
2024-10-15 14:27:44 +0100

    add io comment

--- unassessed_test_1.py
+++ unassessed_test_1.py
@@ -2,6 +2,18 @@ from math import pi
 
 
 def biscuit_cutting():
+    """
+    Input:
+    Please enter the diameter of each biscuit [cm]: 5
+    Please enter the amount of biscuits along length: 6
+    Please enter the amount of biscuits along width: 4
+
+    Output:
+    Radius of each biscuit: 2.50cm
+    Area of each biscuit: 19.63cm^2
+    Total area of biscuit mixture (including spare): 600.00cm^2
+    Biscuits can be made out of spare mixture: 6.0
+    """
     biscuit_diameter = float(input("Please enter the diameter of each biscuit [cm]: "))
     biscuits_along_length = int(input("Please enter the amount of biscuits along length: "))
     biscuits_along_width = int(input("Please enter the amount of biscuits along width: "))
